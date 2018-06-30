b83b30fe9c3662553f52735425f53e415e6ea347
chenshuilong <chenshuilong@gionee.com>
2018-06-30 09:51:25 +0800

    config project

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,16 @@ module Star
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    config.autoload_paths += %W(#{config.root}/lib)
+
+    config.active_record.store_full_sti_class = true
+    config.active_record.default_timezone = :local
+
+    config.time_zone = 'Beijing'
+
+    # Configure the default encoding used in templates for Ruby 1.9.
+    config.encoding = "utf-8"
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
